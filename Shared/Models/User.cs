bf58354dabd0e6746dd65dbb27654dc8f22ffb78
kamkow1 <ikamileq@gmail.com>
2022-02-24 15:18:08 -0800

    basic user data model

---
+++ Shared/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace chattr.Shared.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Login { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
