e6aa90f13a637745b8b085e4f6538345e4bdd841
kamkow1 <ikamileq@gmail.com>
2022-03-04 14:34:29 -0800

    backend login

---
+++ Shared/Models/Chat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace chattr.Shared.Models
+{
+    public class Chat
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public string Topic { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        #region navigation properties
+
+        public virtual ICollection<User> Members { get; set; }
+
+        #endregion
+    }
+}
